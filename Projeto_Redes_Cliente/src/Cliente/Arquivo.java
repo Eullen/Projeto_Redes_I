541466919251a927780f7f19a55cc022b0ca57fd
eullen <eullen.silva@gmail.com>
2017-10-08 16:46:16 -0300

    Estruturando lógica da aplicação

--- Projeto_Redes_Cliente/src/Cliente/Arquivo.java
+++ Projeto_Redes_Cliente/src/Cliente/Arquivo.java
@@ -1,7 +1,9 @@
+package Cliente;
+
 import java.io.Serializable;
 
 /**
- * 
+ *
  */
 
 /**
@@ -9,52 +11,54 @@ import java.io.Serializable;
  *
  */
 public class Arquivo implements Serializable {
-	
+
 	private String nomeArquivo;
+
 	public String getNomeArquivo() {
-		return nomeArquivo;
+		return this.nomeArquivo;
 	}
+
 	public void setNomeArquivo(String nomeArquivo) {
 		this.nomeArquivo = nomeArquivo;
 	}
+
 	public long getTamanhoArquivo() {
-		return tamanhoArquivo;
+		return this.tamanhoArquivo;
 	}
+
 	public void setTamanhoArquivo(long tamanhoArquivo) {
 		this.tamanhoArquivo = tamanhoArquivo;
 	}
-	public String getDiretorioDestino() {
-		return diretorioDestino;
-	}
-	public void setDiretorioDestino(String diretorioDestino) {
-		this.diretorioDestino = diretorioDestino;
-	}
+
 	public byte[] getConteudo() {
-		return conteudo;
+		return this.conteudo;
 	}
+
 	public void setConteudo(byte[] conteudo) {
 		this.conteudo = conteudo;
 	}
+
 	public String getIpServer() {
-		return ipServer;
+		return this.ipServer;
 	}
+
 	public void setIpServer(String ipServer) {
 		this.ipServer = ipServer;
 	}
+
 	public int getPortaServer() {
-		return portaServer;
+		return this.portaServer;
 	}
+
 	public void setPortaServer(int portaServer) {
 		this.portaServer = portaServer;
 	}
+
 	private long tamanhoArquivo;
-	private String diretorioDestino;
 	private byte[] conteudo;
-	
-	//não serializaveis
+
+	// não serializaveis
 	private transient String ipServer;
 	private transient int portaServer;
-	
-	
 
 }
--- Projeto_Redes_Cliente/src/input/Input.java
+++ Projeto_Redes_Cliente/src/input/Input.java
@@ -1,7 +1,10 @@
 package input;
 
 import java.io.File;
+import java.io.FileInputStream;
 
+import Cliente.Arquivo;
+import Cliente.ClienteSocket;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -20,98 +23,114 @@ import javafx.stage.Stage;
 
 // This class will return a path with the file
 
-public class Input extends Application{
+public class Input extends Application {
 
 	private TextField textFieldFileChoosed;
 	private File currentFile = null;
 	private FileChooser fileChooser;
-	
+
 	public static void main(String[] args) {
 		launch(args);
-
 	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
 
-		fileChooser = new FileChooser();
-		fileChooser.setTitle("Escolha um arquivo");
-		
+		this.fileChooser = new FileChooser();
+		this.fileChooser.setTitle("Escolha um arquivo");
+
 		Button sendButton = new Button();
 		sendButton.setText("Enviar");
 		sendButton.setPrefSize(120, 30);
 		sendButton.setOnAction(new EventHandler<ActionEvent>() {
-			 public void handle(ActionEvent event) {
-	             try {
-	            	 if(currentFile.exists()) {
-		            	   //Envia arquivo
-		               }
-	             }
-	             catch(Exception e) {
-	            	showAlert(); 
-	             }
-			 }
+			@Override
+			public void handle(ActionEvent event) {
+				try {
+					if (Input.this.currentFile.exists()) {
+						byte[] conteudo = new byte[(int) Input.this.currentFile.length()];
+						FileInputStream fis = new FileInputStream(Input.this.currentFile);
+						fis.read(conteudo);
+						fis.close();
+
+						Arquivo arq = new Arquivo();
+						arq.setNomeArquivo(Input.this.currentFile.getName());
+						arq.setConteudo(conteudo);
+						arq.setIpServer("127.0.0.1");
+						arq.setPortaServer(6060);
+						arq.setTamanhoArquivo(conteudo.length);
+
+						ClienteSocket client = new ClienteSocket();
+						if (client.enviarArquivo(arq)) {
+							// renderizar mensagem de sucesso
+						} else {
+							// renderizar mensagem de fracasso
+						}
+
+					}
+				} catch (Exception e) {
+					Input.this.showAlert();
+				}
+			}
 		});
-		
+
 		Button chooseButton = new Button();
 		chooseButton.setText("Escolha o arquivo");
 		chooseButton.setPrefSize(120, 30);
 		chooseButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
 			public void handle(ActionEvent event) {
-				chooseFile();
+				Input.this.chooseFile();
 			}
 		});
-		
+
 		// Configurando o grid
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
 		grid.setHgap(10);
 		grid.setVgap(10);
 		grid.setPadding(new Insets(10, 10, 10, 10));
-				
+
 		Label label1 = new Label("Arquivo: ");
 		label1.setPrefWidth(48);
-		
+
 		this.textFieldFileChoosed = new TextField("");
 		this.textFieldFileChoosed.setEditable(false);
-		textFieldFileChoosed.setMinSize(250, 30);
-		
+		this.textFieldFileChoosed.setMinSize(250, 30);
+
 		HBox hbox = new HBox(10);
 		hbox.setAlignment(Pos.CENTER_LEFT);
-		hbox.getChildren().addAll(chooseButton,sendButton);
-		
-		
-		grid.add(label1,0,0);
-		grid.add(textFieldFileChoosed, 1,0,3,1);
-		grid.add(hbox,1,1,4,1);
-        
-        Scene scene = new Scene(grid, 350, 150);
-        stage.setResizable(false);
-        stage.setTitle("Enviar Arquivo");
-        stage.setScene(scene);
-        stage.show();
-        
+		hbox.getChildren().addAll(chooseButton, sendButton);
+
+		grid.add(label1, 0, 0);
+		grid.add(this.textFieldFileChoosed, 1, 0, 3, 1);
+		grid.add(hbox, 1, 1, 4, 1);
+
+		Scene scene = new Scene(grid, 350, 150);
+		stage.setResizable(false);
+		stage.setTitle("Enviar Arquivo");
+		stage.setScene(scene);
+		stage.show();
+
 	}
-	
+
 	private void showAlert() {
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Informe");
 		alert.setHeaderText("Por favor selecione um arquivo para enviar");
 		alert.showAndWait();
 	}
-	
+
 	private void chooseFile() {
 		try {
-			File temp = fileChooser.showOpenDialog(null);
-			if(temp.exists()) {
-				currentFile = temp;
-				String name = currentFile.getName();
-				textFieldFileChoosed.setText(name);
+			File temp = this.fileChooser.showOpenDialog(null);
+			if (temp.exists()) {
+				this.currentFile = temp;
+				String name = this.currentFile.getName();
+				this.textFieldFileChoosed.setText(name);
 			}
-		}
-		catch(Exception e) {
-			if(currentFile == null) {
-				showAlert();
+		} catch (Exception e) {
+			if (this.currentFile == null) {
+				this.showAlert();
 			}
 		}
 	}
